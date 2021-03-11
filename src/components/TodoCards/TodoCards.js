a00e677832ecb70bf3c9f924042abb16522a6fa3
Letorshillen <stom.brehm@t-online.de>
2021-03-11 19:59:23 +0100

    state

---
+++ src/components/TodoCards/TodoCards.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import TodoCard from "./TodoCard/TodoCard";
+
+export default class TodoCards extends Component {
+  render() {
+    return this.props.todos.map((todo, index) => {
+      return (
+        <TodoCard
+          date={todo.date}
+          activity1={todo.activity1}
+          activity2={todo.activity2}
+          activity3={todo.activity3}
+          activity4={todo.activity4}
+          activity5={todo.activity5}
+          activity6={todo.activity6}
+          activity7={todo.activity7}
+        />
+      );
+    });
+  }
+}
